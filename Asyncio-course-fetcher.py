0708a7b9835f10fb134f738928cbda4a2dcad823
Johnson <141822425+kusogame68@users.noreply.github.com>
2025-08-18 23:56:13 +0800

    Optimize variable passing and I/O performance

--- Asyncio-course-fetcher.py
+++ Asyncio-course-fetcher.py
@@ -13,9 +13,9 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoAlertPresentException
 from fake_useragent import UserAgent
+from typing import Optional, Final, Tuple
 from paddleocr import PaddleOCR
 from functools import partial
-from typing import Optional, Final, Tuple
 import concurrent.futures
 import random
 import yaml
@@ -33,19 +33,24 @@ import signal
 """
 uc.Chrome.__del__ = lambda self: None
 
+DRIVER: Optional[uc.Chrome]    = None
+OCR_MODEL: Optional[PaddleOCR] = None
+CONSOLE_LOG : logging.Logger   = None
 URL: str = "https://sss.must.edu.tw/"
 IMG_PATH: str = os.path.join(".", "imgs")
 LOG_FILENAME: Final[str] = "Asyncio.log"
-ACCOUNT: Optional[str]  = None # Loaded from config.yaml
-PASSWORD: Optional[str] = None # Loaded from config.yaml
+ACCOUNT: Optional[str]   = None # Loaded from config.yaml
+PASSWORD: Optional[str]  = None # Loaded from config.yaml
 MAX_RETRY: int = 3
 TIME_COUNTER: float = lambda: round(random.choice(range(2, 10)) * .1, 1)
 
-def setup_log() -> logging.Logger:
+def setup_log() -> None:
+
+    global CONSOLE_LOG
 
     try:
-        console_log: logging.Logger = logging.getLogger("Console_log")
-        console_log.setLevel(logging.DEBUG)
+        CONSOLE_LOG = logging.getLogger("Console_log")
+        CONSOLE_LOG.setLevel(logging.DEBUG)
 
         formatter: logging.Formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s : %(message)s")
 
@@ -57,20 +62,29 @@ def setup_log() -> logging.Logger:
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(formatter)
 
-        if not console_log.handlers:
-            console_log.addHandler(dev_handler)
-            console_log.addHandler(file_handler)
-
-        return console_log
+        if not CONSOLE_LOG.handlers:
+            CONSOLE_LOG.addHandler(dev_handler)
+            CONSOLE_LOG.addHandler(file_handler)
     
     except Exception as e:
-        console_log.error(f"Setup log fail : {e}")
+        CONSOLE_LOG.error(f"Setup log fail : {e}")
         return
 
 def signal_handler(sig, frame) -> None:
     
-    console_log.debug("Using \"ctrl + C\". Exiting program...")
+    CONSOLE_LOG.debug("Using \"ctrl + C\". Exiting program...")
     sys.exit(0)
+
+def setup_ocr() -> None:
+
+    global OCR_MODEL
+
+    try:
+        OCR_MODEL = PaddleOCR(use_textline_orientation = True, lang = "en")
+    
+    except Exception as e:
+        CONSOLE_LOG.error(f"Setup ocr fail : {e}")
+        return
 
 def check_acc_pwd(account: str, password: str) -> Optional[Tuple[str, str]]: 
 
@@ -78,28 +92,28 @@ def check_acc_pwd(account: str, password: str) -> Optional[Tuple[str, str]]:
         Using a regular expression to validate the ACCOUNT and PASSWORD.
     """
     try:
-        if not account or not password:
+        if not all((account, password)):
             raise ValueError("Please input account and password in config.yaml.")
 
         acc_match: re.Match = re.match(r"^[A-Za-z]\d{8}$", account)
         pwd_match: re.Match = re.match(r"^.{6,10}$", password)
 
-        if not acc_match or not pwd_match:
+        if not all((acc_match, pwd_match)):
             raise TypeError("Please double confirm account and password correct or not.")
 
         return account, password
 
     except ValueError as ve:
-        console_log.error(f"{ve}")
+        CONSOLE_LOG.error(f"{ve}")
     except TypeError as te:
-        console_log.error(f"{te}")
+        CONSOLE_LOG.error(f"{te}")
     except Exception as e:
-        console_log.error(f"Check acc and pwd fail : {e}")
+        CONSOLE_LOG.error(f"Check acc and pwd fail : {e}")
     return
 
 def setup_driver() -> Optional[uc.Chrome]:
 
-    global ACCOUNT, PASSWORD
+    global ACCOUNT, PASSWORD, DRIVER
 
     try:
         user_agent     = UserAgent().random
@@ -117,32 +131,28 @@ def setup_driver() -> Optional[uc.Chrome]:
         for config in configs["driver"]:
             chrome_options.add_argument(config)
 
-        driver = uc.Chrome(options = chrome_options)
+        DRIVER = uc.Chrome(options = chrome_options)
 
-        driver.set_page_load_timeout(30)
-        console_log.info("Driver initialized success.")
-        return driver
+        DRIVER.set_page_load_timeout(30)
+        CONSOLE_LOG.info("DRIVER initialized success.")
 
     except Exception as e:
-        console_log.error(f"Driver initialized fail : {e}")
+        CONSOLE_LOG.error(f"DRIVER initialized fail : {e}")
         return
 
-def analysis_element(driver: uc.Chrome,
-                           by: By,
-                           value: str) -> Optional[WebElement]:
+def analysis_element(by: By, value: str) -> Optional[WebElement]:
 
     try:
-        get_element: WebElement = WebDriverWait(driver, 10).until(
+        get_element: WebElement = WebDriverWait(DRIVER, 10).until(
             EC.element_to_be_clickable((by, value))
         )
         return get_element
 
     except Exception as e:
-        console_log.error(f"Analysis_element fail : {e}")
-        return None
+        CONSOLE_LOG.error(f"Analysis element fail : {e}")
+        return
 
-def ocr_img_sync(driver: uc.Chrome,
-                  element: WebElement) -> Optional[str]:
+def ocr_img_sync(element: WebElement) -> Optional[str]:
 
     captcha_path: str   = f"{IMG_PATH}/captcha.png"
     denoising_path: str = f"{IMG_PATH}/denoising.png"
@@ -154,9 +164,6 @@ def ocr_img_sync(driver: uc.Chrome,
         """
         element.screenshot(captcha_path)
         parser_content: str = ""
-
-        ocr = PaddleOCR(use_textline_orientation = True, lang="en")
-        console_log.info("OCR initialized success.")
 
         """ 
             Image preprocessing.
@@ -185,7 +192,7 @@ def ocr_img_sync(driver: uc.Chrome,
         dilated_img             = cv2.bitwise_not(dilated_img)
         cv2.imwrite(dilate_path, dilated_img)
 
-        resaults: list = ocr.predict(dilate_path)
+        resaults: list = OCR_MODEL.predict(dilate_path)
 
         """
             Analyze results data.
@@ -194,17 +201,17 @@ def ocr_img_sync(driver: uc.Chrome,
             parser_content += content
 
         if len(parser_content) == 5:
-            console_log.info(f"OCR captcha success recognized : {parser_content}")
+            CONSOLE_LOG.info(f"OCR captcha success recognized : {parser_content}")
             return parser_content
         else:
-            console_log.warning(f"OCR fail : {parser_content}")
+            CONSOLE_LOG.warning(f"OCR fail : {parser_content}")
             return
 
     except Exception as e:
-        console_log.error(f"OCR_img fail : {e}")
+        CONSOLE_LOG.error(f"OCR img fail : {e}")
         return
 
-async def ocr_img_async(driver: uc.Chrome, element: WebElement) -> Optional[str]:
+async def ocr_img_async(element: WebElement) -> Optional[str]:
 
     """
         Runs a synchronous OCR task in a background thread to avoid blocking the event loop.
@@ -216,53 +223,50 @@ async def ocr_img_async(driver: uc.Chrome, element: WebElement) -> Optional[str]
         try:
             result: Optional[str] = await loop.run_in_executor(
                 executor, 
-                partial(ocr_img_sync, driver, element)
+                partial(ocr_img_sync, element)
             )
 
             return result
 
         except Exception as e:
-            console_log.error(f"OCR async execution fail : {e}")
+            CONSOLE_LOG.error(f"OCR async execution fail : {e}")
             return
 
-async def send_key_to_element(driver: uc.Chrome,
-                              element: WebElement,
-                              content: str) -> Optional[bool]:
+async def send_key_to_element(element: WebElement, content: str) -> Optional[bool]:
 
     try:
         element.clear()
         await asyncio.sleep(TIME_COUNTER())
 
-        actions: ActionChains = ActionChains(driver)
+        actions: ActionChains = ActionChains(DRIVER)
         actions.click(element).send_keys(content).perform()
 
-        console_log.info("Key send success.")
+        CONSOLE_LOG.info("Send key success.")
         return True
 
     except Exception as e:
-        console_log.error(f"Send key fail : {e}")
+        CONSOLE_LOG.error(f"Send key fail : {e}")
         return
 
-async def send_click_to_element(driver: uc.Chrome,
-                                element: WebElement) -> Optional[bool]:
+async def send_click_to_element(element: WebElement) -> Optional[bool]:
 
     try:
-        actions: ActionChains = ActionChains(driver)
+        actions: ActionChains = ActionChains(DRIVER)
         actions.click(element).perform()
 
-        console_log.info("Click send success.")
+        CONSOLE_LOG.info("Send click success.")
         return True
 
     except Exception as e:
-        console_log.error(f"Click fail : {e}")
+        CONSOLE_LOG.error(f"Send click fail : {e}")
         return
 
-async def process_captcha(driver: uc.Chrome) -> Optional[str]:
+async def process_captcha() -> Optional[str]:
 
-    console_log.info("Starting captcha processing...")
+    CONSOLE_LOG.info("Start to captcha process...")
 
     try:
-        vimg_element: Optional[WebElement] = analysis_element(driver, By.ID, "vimg")
+        vimg_element: Optional[WebElement] = analysis_element(By.ID, "vimg")
 
         if not vimg_element:
             return
@@ -270,31 +274,31 @@ async def process_captcha(driver: uc.Chrome) -> Optional[str]:
         """
             Use asynchronous process the OCR.
         """
-        captcha_code = await ocr_img_async(driver, vimg_element)
-        console_log.info("Captcha processing completed.")
+        captcha_code = await ocr_img_async(vimg_element)
+        CONSOLE_LOG.info("Process captcha complete.")
         return captcha_code
 
     except Exception as e:
-        console_log.error(f"Process captcha fail : {e}")
+        CONSOLE_LOG.error(f"Process captcha fail : {e}")
         return
 
-async def input_credentials(driver: uc.Chrome) -> tuple[bool, bool]:
+async def input_credentials() -> tuple[bool, bool]:
 
-    console_log.info("Starting credential input...")
+    CONSOLE_LOG.info("Start to credential input...")
 
     try:
 
-        stdno_element: Optional[WebElement]  = analysis_element(driver, By.NAME, "STDNO")
-        passwd_element: Optional[WebElement] = analysis_element(driver, By.NAME, "PASSWD")
+        stdno_element: Optional[WebElement]  = analysis_element(By.NAME, "STDNO")
+        passwd_element: Optional[WebElement] = analysis_element(By.NAME, "PASSWD")
 
-        if not stdno_element or not passwd_element:
+        if not all((stdno_element, passwd_element)):
             return False, False
 
-        account_task: Optional[bool]  = send_key_to_element(driver, stdno_element, ACCOUNT)
-        password_task: Optional[bool] = send_key_to_element(driver, passwd_element, PASSWORD)
+        account_task: Optional[bool]  = send_key_to_element(stdno_element, ACCOUNT)
+        password_task: Optional[bool] = send_key_to_element(passwd_element, PASSWORD)
 
         """
-            Concurrent processing of account and password.
+            Concurrent process of account and password.
         """
         account_result, password_result = await asyncio.gather(
             account_task, password_task, return_exceptions = True
@@ -303,152 +307,163 @@ async def input_credentials(driver: uc.Chrome) -> tuple[bool, bool]:
         account_success = account_result if not isinstance(account_result, Exception) else False
         password_success = password_result if not isinstance(password_result, Exception) else False
 
-        console_log.info("Credential input completed.")
+        CONSOLE_LOG.info("Input credentials complete.")
         return account_success, password_success
 
     except Exception as e:
-        console_log.error(f"Input credentials fail : {e}")
+        CONSOLE_LOG.error(f"Input credentials fail : {e}")
         return False, False
 
-def handle_alert(driver: uc.Chrome) -> Optional[bool]:
+def alert_handler() -> Optional[bool]:
 
     try:
-        alert: WebElement = driver.switch_to.alert
+        alert: WebElement = DRIVER.switch_to.alert
         msg: str          = alert.text
-        console_log.warning(f"Alert detected : {msg}")
+        CONSOLE_LOG.warning(f"Alert detected : {msg}")
         alert.accept()
         return True
 
     except NoAlertPresentException:
         return
 
-async def login_attempt(driver: uc.Chrome) -> Optional[bool]:
+async def login_attempt() -> Optional[bool]:
 
     try:
-        console_log.info("Starting concurrent operations...")
+        CONSOLE_LOG.info("Start to concurrent operations...")
 
-        account_passwork_input: tuple[bool, bool] = input_credentials(driver)
-        capcha_input: Optional[str]               = process_captcha(driver)
+        account_passwork_input: tuple[bool, bool] = input_credentials()
+        capcha_input: Optional[str]               = process_captcha()
 
         captcha_code, (account_success, password_success) = await asyncio.gather(
             capcha_input, account_passwork_input
         )
 
-        console_log.info("Concurrent operations completed.")
+        CONSOLE_LOG.info("Concurrent operations complete.")
 
-        if not account_success or not password_success:
-            console_log.warning("Fail to input account and password.")
+        if not all((account_success, password_success)):
+            CONSOLE_LOG.warning("Fail to input account and password.")
             return
 
         if not captcha_code:
-            vimg_element: Optional[WebElement] = analysis_element(driver, By.ID, "vimg")
+            vimg_element: Optional[WebElement] = analysis_element(By.ID, "vimg")
 
             if vimg_element:
-                await send_click_to_element(driver, vimg_element)
-                console_log.warning("Fail process captcha.")
+                await send_click_to_element(vimg_element)
+                CONSOLE_LOG.warning("Fail to process captcha.")
                 await asyncio.sleep(TIME_COUNTER())
             return
 
-        captcha_input: Optional[WebElement] = analysis_element(driver, By.ID, "ValidCode_login")
-        submit_button: Optional[WebElement] = analysis_element(driver, By.CLASS_NAME, "btn-primary")
+        captcha_input: Optional[WebElement] = analysis_element(By.ID, "ValidCode_login")
+        submit_button: Optional[WebElement] = analysis_element(By.CLASS_NAME, "btn-primary")
 
-        if not captcha_input or not submit_button:
-            console_log.error("Fail to analyze input of captcha and submit.")
+        if not all((captcha_input, submit_button)):
+            CONSOLE_LOG.error("Fail to analyze input of captcha and submit.")
             return
 
-        await send_key_to_element(driver, captcha_input, captcha_code)
-        await send_click_to_element(driver, submit_button)        
+        await send_key_to_element(captcha_input, captcha_code)
+        await send_click_to_element(submit_button)        
         await asyncio.sleep(TIME_COUNTER())
 
-        if handle_alert(driver):
+        if alert_handler():
             return
 
-        if "news.asp" in driver.current_url:
-            console_log.info("Login success.")
+        if "news.asp" in DRIVER.current_url:
+            CONSOLE_LOG.info("Login success.")
             return True
         else:
-            console_log.warning("Login fail.")
+            CONSOLE_LOG.warning("Login fail.")
             return
 
     except Exception as e:
-        console_log.error(f"Login attempt fail : {e}")
+        CONSOLE_LOG.error(f"Login attempt fail : {e}")
         return
 
-async def login_page(driver: uc.Chrome) -> Optional[bool]:
+async def login_page() -> Optional[bool]:
 
     try:
-        driver.get(URL)
+        DRIVER.get(URL)
 
         for _ in range(MAX_RETRY):
-            console_log.info(f"Login attempt {_ + 1} / {MAX_RETRY}")
+            CONSOLE_LOG.info(f"Login attempt {_ + 1} / {MAX_RETRY}")
 
-            if await login_attempt(driver):
+            if await login_attempt():
                 return True
 
             if _ < MAX_RETRY - 1 :
-                console_log.warning("Login fail, retrying...")
+                CONSOLE_LOG.warning("Login fail, retrying...")
                 await asyncio.sleep(TIME_COUNTER())
         return
 
     except Exception as e:
-        console_log.error(f"Login page fail : {e}")
+        CONSOLE_LOG.error(f"Login page fail : {e}")
         return
 
-async def navigate_to_course(driver: uc.Chrome) -> None:
-    
+async def navigate_to_course() -> None:
+
     try:
         """
             Executing it twice is to resolve the advertising pop-up when loggin success.
         """
-        personal_info: Optional[WebElement] = analysis_element(driver, By.ID, "personalinfo")
-        await send_click_to_element(driver, personal_info)
-        await send_click_to_element(driver, personal_info)
+        personal_info: Optional[WebElement] = analysis_element(By.ID, "personalinfo")
+        await send_click_to_element(personal_info)
+        await send_click_to_element(personal_info)
         await asyncio.sleep(TIME_COUNTER())
 
-        course: Optional[WebElement] = analysis_element(driver, By.ID, "class")
-        await send_click_to_element(driver, course)
+        course: Optional[WebElement] = analysis_element(By.ID, "class")
+        await send_click_to_element(course)
 
-        new_semester: Optional[WebElement] = analysis_element(driver, By.ID, "c2")
-        await send_click_to_element(driver, new_semester)
+        new_semester: Optional[WebElement] = analysis_element(By.ID, "c2")
+        await send_click_to_element(new_semester)
 
-        console_log.info("Navigate to course success.")
+        CONSOLE_LOG.info("Navigate to course success.")
 
     except Exception as e:
-        console_log.error(f"Navigate to course fail : {e}")
+        CONSOLE_LOG.error(f"Navigate to course fail : {e}")
 
     return
 
 async def main() -> None:
 
+    global DRIVER, OCR_MODEL, CONSOLE_LOG
+
     os.makedirs("imgs", exist_ok = True)
-    driver: Optional[uc.Chrome] = None
 
     try:
         signal.signal(signal.SIGINT, signal_handler)
-        driver: Optional[uc.Chrome] = setup_driver()
 
-        if not driver:
-            console_log.error("Driver start fail. Exiting program...")
+        loop = asyncio.get_event_loop()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            await asyncio.gather(
+                loop.run_in_executor(executor, setup_driver),
+                loop.run_in_executor(executor, setup_ocr)
+            )
+
+        if not all((DRIVER, OCR_MODEL)):
+            CONSOLE_LOG.error("DRIVER or OCR_MODEL init fail. Exiting program...")
             return
-            
-        await login_page(driver)
+
+        await login_page()
         await asyncio.sleep(TIME_COUNTER())
         
-        if "news.asp" not in driver.current_url:
-            console_log.error("All login attempts fail. Exiting program...")
+        if "news.asp" not in DRIVER.current_url:
+            CONSOLE_LOG.error("All login attempts fail. Exiting program...")
             return 
 
-        await navigate_to_course(driver)
+        await navigate_to_course()
         await asyncio.sleep(5)
 
     except Exception as e:
-        console_log.error(f"Workflow fail : {e}")
+        CONSOLE_LOG.error(f"Workflow fail : {e}")
 
     finally:
-        if driver is not None:
-            driver.quit()
-            console_log.info("Driver quit.")
+        if DRIVER is not None:
+            DRIVER.quit()
+            CONSOLE_LOG.info("DRIVER quit.")
+
+        DRIVER      = None
+        OCR_MODEL   = None
+        CONSOLE_LOG = None
 
 if __name__ == "__main__":
-    console_log = setup_log()
+    setup_log()
     asyncio.run(main())
